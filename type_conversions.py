9d64a99931d0e99e28afe6c6b365f16a13c9938c
Ryan P. Hopkins <ryan.hopkins2@snhu.edu>
2024-05-04 23:14:41 -0400

    These are the lastest bootcamp updates

---
+++ type_conversions.py
@@ -0,0 +1,17 @@
+a = 'hi'
+b = 2
+c = 2.0
+d = True
+
+print(f'a: {type(a)}')
+print(f'b: {type(b)}')
+print(f'c: {type(c)}')
+print(f'd: {type(d)}')
+
+# str()
+my_string1 = str(2)
+my_string2 = str(2.7)
+print(my_string1)
+print(my_string2)
+print(type(my_string1))
+print(type(my_string2))
