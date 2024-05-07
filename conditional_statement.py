e5a098fe213ce329cd47c95ed4661f6fb53d1b8b
Ryan P. Hopkins <ryan.hopkins2@snhu.edu>
2024-05-06 22:39:58 -0400

    Updated logic AND created conditional statements (if, elif, else)

---
+++ conditional_statement.py
@@ -0,0 +1,11 @@
+print()
+# Conditional Statements
+a = 5
+if  a > 6: # type in logical expression after keyword 'if', which is known as a condition
+    print('That is correct!') # This message prints out 'if' the expression is True, 4 or less
+elif a == 3: 
+    print('Got it on the second try!') # If I place a 5 on this line, then the expression is True...
+    print()                                 # and this message prints
+else: 
+    print('Try again.') # If neither statements are true, then this message prints
+    print()
