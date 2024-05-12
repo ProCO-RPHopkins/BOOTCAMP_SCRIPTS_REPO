847821d76e2809957065a0e45bf098710c879f6f
Ryan P. Hopkins <ryan.hopkins2@snhu.edu>
2024-05-12 16:06:11 -0400

    Lists and indices

---
+++ index_lists.py
@@ -0,0 +1,14 @@
+# Indexing begins at 0, so when I print 2, it will give me the third element
+names = ['Jane', 'John', 'Ryan', 'Julie', 'Jolene']
+
+print(names[2])
+
+# Replacing elements in the list using indices
+
+names[2] = "Jack"
+print(names[2])
+print(names)
+print()
+
+# When starting from the back of the list, index numbers begin with -1, -2, -3, etc.
+print(names[-2])
