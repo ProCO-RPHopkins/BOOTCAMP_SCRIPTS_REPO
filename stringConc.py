eec06e61bca8dbcd6b7df36bb2df2ef22d4fc175
Ryan P. Hopkins <ryan.hopkins2@snhu.edu>
2024-05-14 18:36:50 -0400

    modified StringConc

--- stringConc.py
+++ stringConc.py
@@ -1,4 +1,4 @@
 #Concatinating str and int by using "str(age)" instead of "int(age)"
 name = "Jonny Doe"
 age = 40
-print("My name is" + name + "and I am" + str(age), "years old.")
+print("My name is " + name + "and I am " + str(age), "years old.")
