0cbe6ed21bd3e1a8c3dbe3fbf57c6adfda56dfc2
Ryan P. Hopkins <ryan.hopkins2@snhu.edu>
2024-06-06 18:31:52 -0400

    simplifying python & vs settings

--- 1_leetcode.py
+++ 1_leetcode.py
@@ -1,15 +1,28 @@
-lists = ["a", "b", "c", "d"]
-
-for l in lists:
-    print(l)
-
 
 s = "cabcacdd"
-count = 0
 
-for letters in s:
-    if letters == "a":
-        count += 1
-        result = count
+def count_letters(s, letter):
+    return sum(1 for l in s if l == letter)
 
-print(f"There are {result} 'a's in the list.")
+count_a = count_letters(s, "a")
+count_b = count_letters(s, "b")
+count_c = count_letters(s, "c")
+count_d = count_letters(s, "d")
+
+print(f"There are {count_a} a's in the string.")
+print(f"There are {count_b} b's in the string.")
+print(f"There are {count_c} c's in the string.")
+print(f"There are {count_d} d's in the string.")
+print()
+print()
+
+from collections import Counter
+
+s = "cabcacdd"
+counts = Counter(s)
+
+for letter in 'abcd':
+    print(f"There are {counts[letter]} {letter}'s in the string.")
+
+
+
